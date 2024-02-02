bb28c2d0bd3da106388b6314540870693580f09d
Evan Shortiss <evanshortiss@gmail.com>
2024-02-01 19:55:13 -0800

    fix: reading database url

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,10 @@ neonConfig.fetchConnectionCache = true
 
 export function getDatabase () {
   // Workaround until Neon Vercel integration adds this query parameter
-  const url = new URL(process.env.DATABASE_URL!)
+  if (!process.env.DATABASE_URL) {
+    throw new Error('process.env.DATABASE_URL was missing')
+  }
+  const url = new URL(process.env.DATABASE_URL)
   url.searchParams.set('sslmode', 'require')
 
   const sql = neon(url.toString());
